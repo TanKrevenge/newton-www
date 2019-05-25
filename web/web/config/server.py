806f3c97b32ce1d69ec9fe5963f3f9e46a85848f
tiankai <7871735978@qq.com>
2019-05-25 14:29:01 +0800

    adjust style and transfer it to english

--- web/web/config/server.py
+++ web/web/config/server.py
@@ -99,11 +99,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'newton_www',
-        'USER': 'root',
-        'PASSWORD': '',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'newton_www',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    # },
 }
 
 # Cache
