8262669c8fb002b7b0b8903375694b8fc89c255a
tiankai <7871735978@qq.com>
2019-05-25 14:36:44 +0800

    adjust the wrong push of the server.py

--- web/web/config/server.py
+++ web/web/config/server.py
@@ -99,15 +99,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'newton_www',
-    #     'USER': 'root',
-    #     'PASSWORD': '',
-    # },
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newton_www',
+        'USER': 'root',
+        'PASSWORD': '',
+    },
 }
 
 # Cache
